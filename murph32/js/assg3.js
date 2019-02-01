0b9bb207c64e726f820a96d9917d5e3906ddbd12
Amber <ajmurphy505@gmail.com>
2019-02-01 17:43:41 -0500

    assignment 3

---
+++ murph32/js/assg3.js
@@ -0,0 +1,15 @@
+ /**********
+Date: 2019-01-25
+Author: Amber Murphy
+Class: CITW 165
+Exercise: Exercise #3
+**********/
+var title = "Assignment 3 - Working with Var";
+
+var ipsumText = "We can fix anything. I thought today we would make a happy little stream that's just running through the woods here. That's a son of a gun of a cloud. The shadows are just like the highlights, but we're going in the opposite direction. Of course he's a happy little stone, cause we don't have any other kind. A fan brush can be your best friend.";
+
+var as3Title = document.getElementById('pageHeader');
+as3Title.textContent = title;
+
+var as3Quote = document.getElementById('ibsumQuote');
+as3Quote.textContent = ipsumText;
