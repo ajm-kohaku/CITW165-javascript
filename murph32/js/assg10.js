94cc167ec65d7f5b5c2174da49b8a5bef7c0e470
Amber <ajmurphy505@gmail.com>
2019-04-25 08:06:19 -0400

    assignment 10

---
+++ murph32/js/assg10.js
@@ -0,0 +1,43 @@
+/**************************
+    Date: 2019-04-25
+    Author: Amber Murphy
+    Class: CITW 165
+    Exercise: Exercise #10
+    code modified from book example
+**************************/
+
+// validate signup form on keyup and submit
+$("#email_form").validate({
+    rules: {
+        first_name: "required",
+        last_name: "required",
+        email: {
+            required: true,
+            email: true
+        },
+        email_confirm: {
+            required: true,
+            equalTo: "#email"
+        },
+        zip: {
+            required: true,
+            email: true,
+            minlength: 5
+        }
+    },
+    messages: {
+        first_name: "Please enter your firstname",
+        last_name: "Please enter your lastname",
+        email: {
+            required: "Please provide an email address"
+        },
+        email_confirm: {
+            required: "Please provide an email address",
+            equalTo: "Please enter the same email as above"
+        },
+        zip: "Please enter a valid zip code",
+        minlength: "Your zip code must be at least 5 characters long"
+    }
+});
+
+$("#email_form").validate();
